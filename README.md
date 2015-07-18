894fe0740943f3e2e118401fb2d9d679631b303e
Volker Braun <vbraun.name@gmail.com>
2015-07-18 23:20:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+Sage Dev Webapp
+===============
+
+
+- app: Python backend source
+- www: Frontend source code (Polymer)
+
+
---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+### Bootstrap the application, starting from python + virtualenv
+#
+# /local             Python virtualenv
+# /node_modules      node.js external modules
+# /bower_components  Bower external modules
+
+set -e
+
+virtualenv --python=python3.4 local
+source ./local/bin/activate
+
+./local/bin/pip install --upgrade pip
+./local/bin/pip install -r requirements.txt
+./local/bin/nodeenv --python-virtualenv
+./local/bin/npm install
+./node_modules/.bin/bower install
