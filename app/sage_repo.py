6d3d09cd2d83a9f0220b61bee169409613c99192
Volker Braun <vbraun.name@gmail.com>
2015-08-08 19:56:23 +0200

    make sure the sage repo exists before importing from it

--- app/sage_repo.py
+++ app/sage_repo.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 from app.config import config
+from app.git_repo import app_repo
 
 def _prepare():
     sage_bootstrap_path = os.path.join(config.data_files.repo.path, 'build')
